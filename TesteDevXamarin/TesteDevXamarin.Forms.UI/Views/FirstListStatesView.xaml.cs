3d6d3e1a5dcd3504604a1b2e12cc9b8cf6138005
davimonteiro7 <davi.monteiro7@gmail.com>
2019-12-14 15:44:10 -0300

    Add TestDevXamarin.UI, here we implement User Interface Views.

---
+++ TesteDevXamarin/TesteDevXamarin.Forms.UI/Views/FirstListStatesView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.Forms.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TesteDevXamarin.Core.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TesteDevXamarin.Forms.UI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstListStatesView : MvxContentPage<FirstListStatesViewModel>
+    {
+        public FirstListStatesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
